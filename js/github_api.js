5a0f5e3f07f04e6fe099430db8f887365ffde124
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-18 10:44:48 -0700

    display information about user repositories in two columns

--- js/github_api.js
+++ js/github_api.js
@@ -11,11 +11,15 @@ exports.getRepos = function(searchUser, apiKey){
   });
   $.get('https://api.github.com/users/' + searchUser + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $("#user-repositories").empty();
-    for (var i = 0; i < response.length; i++) {
-      $("#user-repositories").append("<li>" + response[i].name + ": " + response[i].description + "</li>");
+    $(".user-repositories").empty();
+    for (var i = 0; i < 15; i++) {
+      $("#col-1").append("<li><a href=" + response[i].html_url + ">" + response[i].name + "</a>" + ": " + response[i].description + "</li>");
+    }
+    for (var i2 = 15; i2 < 30; i2++) {
+      $("#col-2").append("<li><a href=" + response[i2].html_url + ">" + response[i2].name + "</a>" + ": " + response[i2].description + "</li>");
     }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
+  $(".displays").show();
 };
