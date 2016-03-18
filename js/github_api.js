8f1b852336457c41c67dc94cbd9002115ef89039
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-18 10:11:28 -0700

    show name and description for user's public repositories

--- js/github_api.js
+++ js/github_api.js
@@ -6,14 +6,16 @@ exports.getRepos = function(searchUser, apiKey){
     $("#user-location").html("<h4>Location: " + response.location + "</h4>");
     $("#user-url").html("<h4><a href=" + response.html_url + ">Github</a></h4>");
     $("#user-avatar").html("<img src=" + response.avatar_url + ">");
-    // for(repo in response.)
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
-  // $.get('https://api.github.com/users/' + searchUser + '/repos?access_token=' + apiKey).then(function(response){
-  //   console.log(response);
-  //   // for(repo in response.)
-  // }).fail(function(error){
-  //   console.log(error.responseJSON.message);
-  // });
+  $.get('https://api.github.com/users/' + searchUser + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    $("#user-repositories").empty();
+    for (var i = 0; i < response.length; i++) {
+      $("#user-repositories").append("<li>" + response[i].name + ": " + response[i].description + "</li>");
+    }
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
 };
